6851e2e3be070898a6399d2c891da252a33368e8
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-03-03 12:03:02 -0700

    first commit"

---
+++ src/app/index.ts
@@ -0,0 +1,13 @@
+import colors from 'colors';
+
+import app from 'src/server/index.js'
+
+const port = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log(colors.blue.bold(`Server is running on port ${port}`));
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
---
+++ src/server/index.ts
@@ -0,0 +1,28 @@
+import colors from 'colors'
+import express from 'express'
+
+import { router } from '../router/index.js'
+import { db } from '../config/index.js'
+
+
+//conectar a DB
+async function connectDb(){
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log(colors.green.bold('Base de datos conectada'));
+      }
+      catch (error) {
+        console.error(colors.red('Error en conectar la base de datos'));
+
+      }
+}
+
+connectDb()
+const app = express();
+
+app.use('/api/productos',router);
+
+
+
+export default app
