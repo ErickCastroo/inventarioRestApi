8fa4184692f1793166d1d4b155e04878fba70309
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-03-04 11:27:02 -0700

    Add error handling middleware and refactor product creation handler

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,17 +1,13 @@
 import { Request, Response } from 'express'
-import { validationResult } from 'express-validator'
 
 import Product from '../models/Product.model.js'
 
-export const createProduct = async (req: Request, res: Response) => {
+const createProduct = async (req: Request, res: Response) => {
   
-  //checar si hay errores en la validacion y regresarlos
-  let errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
-  } 
-  
+
   //crear el producto
   const producto = await Product.create(req.body)
   res.json({ data: producto })
 }
+
+export { createProduct }
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import { body } from 'express-validator'
 
 import { createProduct } from 'src/handlers/products.js'
+import { HandlerErrors } from 'src/middleware/index.js'
+
 
 
 const router = Router()
@@ -18,11 +20,12 @@ router.post('/products',
     .custom(value => value > 0),
 
   body('cantidadDisponible', 'La cantidad es requerida')
-  .isNumeric()
-  .notEmpty()
-  .custom(value => value > 0),
-
-  createProduct)
+    .isNumeric()
+    .notEmpty()
+    .custom(value => value > 0),
+  HandlerErrors,
+  createProduct
+)
 
 
 export { router }
