3fff6bd0d06ce4a6abd999a27e6fe00e9fa7d911
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-03-05 12:03:50 -0700

    Add Jest configuration and initial test cases for server functionality

---
+++ src/__test__/server.test.ts
@@ -0,0 +1,9 @@
+describe('primer test', () => {
+  it('revisar si 1 + 1 sean 2', () => {
+    expect(1 + 1).toBe(2)
+  })
+
+  it('revisar si 1 + 1 sean 2', () => {
+    expect(1 + 1).not.toBe(3)
+  })
+})
